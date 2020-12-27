464d720db88db54c2a197b4bc66caf3e055d3668
forward22222 <szdforward@163.com>
2020-12-27 21:35:48 +0800

    提交

--- course_test/Test02_string_test.py
+++ course_test/Test02_string_test.py
@@ -9,7 +9,7 @@ print(str[2:])      # 打印从第3个字母到末尾 llooWorld!
 print(str * 2)      # 字符串重复2次 HellooWorld!HellooWorld!
 print(str + "TEST")  # 字符串拼接 HellooWorld!TEST  注意这里只有str类型的才可以使用“+”号  其他类型使用的话会报错
 
-#格式化输出字符串
+#格式化输出字符串 Python字符串格式化 见https://www.runoob.com/python3/python3-string.html文档中也包含：f-string 是 python3.6 之后版本添加的
 sss ="Hello,%s,%s enough for ya?"
 values =('world','Hot')
 print(sss %values) # Hello,world,Hot enough for ya?  注意这里在sss后面加空格或者不加空格都是可以的
@@ -20,3 +20,19 @@ print(s.substitute(lang='Python',howmany=3)) # There are 3 Python Quotation Symb
 
 ##字符串 join
 print("##".join(("a","b"))) # 结果为： a##b
+
+## 字符串正则匹配
+import re
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print("matchObj.group() : ", matchObj.group())
+   print("matchObj.group(1) : ", matchObj.group(1))
+   print("matchObj.group(2) : ", matchObj.group(2))
+else:
+   print("No match!!")
+
+
+
--- course_test/Test08_01_file_op.py
+++ course_test/Test08_01_file_op.py
@@ -1,45 +1,36 @@
 #coding=utf-8
 
 """
-文件的操作
+文件的操作 找到文件中匹配的那一行
 """
-# import os
+def funRe(line,tbl):
+    matchObj  = re.match('.*"product":"(.*?)"'+'.*'+tbl+'.*',line)
+    if matchObj:
+        print(matchObj.group(1) + '\t' + tbl)
+        return True
+    else:
+        return False
+import os
+import re
 # files = os.listdir("F:\python_resources\day37-python\day37课件资料")
 # logfile=[f for f in files if f.endswith('.exe') ]
 # print(logfile) # ['Anaconda3-2.4.1-Windows-x86_64.exe', 'pycharm-community-5.0.2.exe', 'python-3.8.2.exe', 'python-3.8.5-amd64.exe']
-
-# fileHandler = open('F:/temp/hello.py', 'a+')	#以读写方式处理文件IO 文件路径需要使用反斜杠
-# fileHandler.seek(0)
-# 读取一行
-# line = fileHandler.readline()
-# while line:
-# 	print(line)
-# 	line = fileHandler.readline()
-# fileHandler.close
-
-
-# fileHandler = open('F:/temp/hello.py', 'a+')	#以读写方式处理文件IO
-# fileHandler.seek(0)
-# #读取整个文件
-# contents = fileHandler.read()
-# print(contents,type(contents)) # print("hello world!") <class 'str'>
-# #读取所有行,再逐行输出
-# fileHandler.seek(0)
-# lines = fileHandler.readlines()
-# print(type(lines)) # <class 'list'>
-# for line  in lines:
-# 	print(line)
-# #当前文件指针的位置
-# print(fileHandler.tell()) # 21
-# fileHandler.close
-
-
-# 写入文件
-fileHandler = open('F:/temp/hello.py','a+')   #或者调用open()函数
-fileHandler.write("\r\n")
-fileHandler.write("thank you")
+list = ["deskdict", "dict_banner", "dict_comment_server", "dict_community_server", "dict_listening", "dict_markets", "dict_push_server", "dict_suggest_server", "dictblog", "dictinfoline", "dictinfoline_recommendid", "dictmessage", "dictprofile", "dictsearch", "dictsearch_dict_noecceresult", "dictsearch_q", "dictweb_pageview_aged", "dictwordbook", "dict_app_embedded_wap", "dict_app_embedded_wap_paidu", "dict_app_embedded_wap_pen_linkage", "dict_baike_server", "dict_community", "dict_data_server", "dict_search_server", "dict_server", "dict_sign", "dict_usertask_server", "dict_vip", "dict_voice", "dict_yiju", "kid_dict_server", "kid_dict_server_all", "kiddict_market", "linuxdict", "mobile_mobile_dict_nps", "mobiledictclient_android_bg", "mobiledictclient_ipad", "mobiledictclient_perf", "mobiledictindexdetail", "mobiledictlocation_android", "mobiledictlocation_iphonepro", "picdictserver", "ugc_dict"]
+fileHandler = open('F:/temp/dso.txt', 'a+')	#以读写方式处理文件IO 文件路径需要使用反斜杠
 fileHandler.seek(0)
-contents = fileHandler.read()
-print(contents)
+# # 读取一行
+line = fileHandler.readline()
+while line:
+    # print(line)
+    for tbl in list:
+        if(funRe(line,tbl)):
+            break
+    line = fileHandler.readline()
 fileHandler.close
+
+
+
+
+
+
 
