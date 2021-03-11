1c7bfb44f5431f639be389c9ef70252eb2677d8f
forward22222 <szdforward@163.com>
2021-03-12 01:28:54 +0800

    索引和切片2

---
+++ data_analysis/numpy/05_gaojisuoyin.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@author:Administrator
+@file:05_gaojisuoyin.py
+@time:2021/03/12
+"""
+import numpy as  np
+a = np.arange(12)**2
+print(a)#[  0   1   4   9  16  25  36  49  64  81 100 121]
